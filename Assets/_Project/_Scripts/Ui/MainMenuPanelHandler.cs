4b80a7e6cae2b0add63e3aaadd2254dea4c48ab1
Ali Qadir <94987050+ALI-QADIR@users.noreply.github.com>
2024-12-28 16:47:44 +0530

    Main Panel Animation

--- Assets/_Project/_Scripts/Ui/MainMenuPanelHandler.cs
+++ Assets/_Project/_Scripts/Ui/MainMenuPanelHandler.cs
@@ -2,5 +2,10 @@
 {
 	public class MainMenuPanelHandler : PanelHandler
 	{
+		protected override void Awake()
+		{
+			base.Awake();
+			OpenPanel();
+		}
 	}
 }
--- Assets/_Project/_Scripts/Ui/PanelHandler.cs
+++ Assets/_Project/_Scripts/Ui/PanelHandler.cs
@@ -1,19 +1,65 @@
 ﻿using System.Collections;
+using PrimeTween;
 using Smash.System;
 using TripleA.EventSystem;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 namespace Smash.Ui
 {
 	public abstract class PanelHandler : EventListener<UiEventArgs>
 	{
+		[Header("Animation")] 
+		[SerializeField] private float m_duration;
+		[SerializeField] private Ease m_ease;
+		[SerializeField] protected Transform _closeTransform;
+		[SerializeField] protected Transform _openTransform;
+
+		[Space(10)]
 		[SerializeField] private ButtonSelectionHandler m_primaryButton;
+
+		protected Sequence _openSequence, _closeSequence;
+		protected Transform _tr;
+		
+		protected override void Awake()
+		{
+			base.Awake();
+			_tr = transform;
+			_tr.position = _closeTransform.position;
+			gameObject.SetActive(false);
+		}
 
 		protected virtual IEnumerator Start()
 		{
 			yield return null;
 			EventSystem.current.SetSelectedGameObject(m_primaryButton.gameObject);
 		}
+
+		public virtual void OpenPanel() => Show();
+		public virtual void ClosePanel() => Hide();
+
+		protected virtual void Show()
+		{
+			if (_openSequence.isAlive) return;
+			
+			gameObject.SetActive(true);
+			_openSequence = Sequence.Create()
+				.Group(Tween.Position(target: _tr, startValue: _closeTransform.position,
+					endValue: _openTransform.position, duration: m_duration, ease: m_ease));
+		}
+
+		protected virtual void Hide()
+		{
+			if (_closeSequence.isAlive) return;
+			
+			_closeSequence = Sequence.Create()
+				.Group(Tween.Position(target: _tr, startValue: _openTransform.position,
+					endValue: _closeTransform.position, duration: m_duration, ease: m_ease));
+			
+			_closeSequence.OnComplete(target: this, target => target.DisableGameObject());
+		}
+
+		private void DisableGameObject() => gameObject.SetActive(false);
 	}
 }
