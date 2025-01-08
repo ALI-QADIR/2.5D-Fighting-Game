594014d508f80b0cf42b52a4e357a3ce95747186
Ali Qadir <94987050+ALI-QADIR@users.noreply.github.com>
2025-01-08 18:50:35 +0530

    Unity Auth

---
+++ Assets/_Project/_Scripts/System/PlayerAuthentication.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using System.Threading.Tasks;
+using TripleA.Singletons;
+
+namespace Smash.System
+{
+	public class PlayerAuthentication : PersistentSingleton<PlayerAuthentication>
+	{
+		private PlayerSignInData m_playerSignInData;
+		public string PlayerId => m_playerSignInData?.Id;
+		public string AccessToken => m_playerSignInData?.AccessToken;
+		
+		private bool m_isServiceInitialised;
+
+		public bool IsSignedIn()
+		{
+			if (!m_isServiceInitialised) return false;
+			return AuthenticationService.Instance.IsSignedIn;
+		}
+
+		protected override void Awake()
+		{
+			base.Awake();
+			m_isServiceInitialised = false;
+		}
+
+		public async Task<bool> InitializeUnityAuthentication()
+		{
+			if (m_isServiceInitialised) return true;
+			Debug.Log("Initializing Unity Authentication");
+			try
+			{
+				await UnityServices.InitializeAsync();
+
+				Debug.Log("Setup Events");
+				SetUpEvents();
+				m_isServiceInitialised = true;
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("Unity Authentication Failed " + e);
+				m_isServiceInitialised = false;
+			}
+
+			return m_isServiceInitialised;
+		}
+
+		public async Task<bool> TryAnonSignin()
+		{
+			Debug.Log("Try Sign In");
+			try
+			{
+				await AuthenticationService.Instance.SignInAnonymouslyAsync();
+				m_playerSignInData = CreatePlayerData();
+				Debug.Log($"Name {m_playerSignInData.PlayerName}\nID {m_playerSignInData.Id}");
+				return true;
+			}
+			catch (ServicesInitializationException e)
+			{
+				Debug.Log("Initialising Service" + e);
+				await InitializeUnityAuthentication();
+				return false;
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("Sign In Failed " + e);
+				return false;
+			}
+		}
+
+		private PlayerSignInData CreatePlayerData() => new (AuthenticationService.Instance.PlayerId,
+			AuthenticationService.Instance.AccessToken, "anon");
+
+		private void SetUpEvents()
+		{
+			AuthenticationService.Instance.SignedIn += InstanceOnSignedIn;
+			AuthenticationService.Instance.SignedOut += InstanceOnSignedOut;
+			AuthenticationService.Instance.SignInFailed += InstanceOnSignInFailed;
+		}
+
+		private void InstanceOnSignInFailed(RequestFailedException obj)
+		{
+			Debug.Log("sign in failed");
+		}
+
+		private void InstanceOnSignedOut()
+		{
+			Debug.Log("Signed Out");
+		}
+
+		private void InstanceOnSignedIn()
+		{
+			Debug.Log("Signed In");
+		}
+	}
+}
---
+++ Assets/_Project/_Scripts/System/PlayerSignInData.cs
@@ -0,0 +1,18 @@
+﻿namespace Smash.System
+{
+	public class PlayerSignInData
+	{
+		private readonly string m_id, m_accessToken;
+		public string PlayerName { get; private set; }
+		public string Id => m_id;
+		public string AccessToken => m_accessToken;
+		public PlayerSignInData(string id, string accessToken, string playerName)
+		{
+			m_id = id;
+			m_accessToken = accessToken;
+			PlayerName = playerName;
+		}
+		
+		public void SetPlayerName(string name) => PlayerName = name;
+	}
+}
