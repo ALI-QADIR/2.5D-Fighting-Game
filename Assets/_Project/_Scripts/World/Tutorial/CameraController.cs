5c0e9ee9d6c4097689f1e414c97ed46201bfa21b
Ali Qadir <94987050+ALI-QADIR@users.noreply.github.com>
2025-01-11 17:39:05 +0530

    Tutorial Camera Controler

---
+++ Assets/_Project/_Scripts/World/Tutorial/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PrimeTween;
+using UnityEngine;
+
+namespace Smash.World.Tutorial
+{
+	public class CameraController : MonoBehaviour
+	{
+		private Transform m_tr;
+		[SerializeField] private float m_duration = 0.5f;
+		[SerializeField] private Ease m_ease = Ease.InOutSine;
+
+		private void Awake()
+		{
+			m_tr ??= transform;
+		}
+
+		private void OnEnable()
+		{
+			Area.OnAreaEnter += OnAreaEnter;
+		}
+
+		private void OnDisable()
+		{
+			Area.OnAreaEnter -= OnAreaEnter;
+		}
+
+		private void OnAreaEnter(Vector3 obj)
+		{
+			Tween.Position(m_tr, obj, m_duration, m_ease);
+		}
+	}
+}
